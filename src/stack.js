2e4a904669dce2da129208f1297fe53c550e7fdd
TonicB <a.sharafiev@gmail.com>
2023-04-15 17:09:17 +0200

    feat: solve stack and queue

--- src/stack.js
+++ src/stack.js
@@ -12,20 +12,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
-class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Stack extends Array {
+  push(element) {
+    this[this.length] = element
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   return super.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this[this.length-1]
   }
 }
 
